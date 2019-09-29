dc80b985b80b5b335bc317e6020bad7b9a47597c
Vinícius Adamy <34045326+Adamy96@users.noreply.github.com>
2019-09-28 21:42:33 -0300

    Update Map.js (fireBall)
    Responsive fireBall size & initial speedY.

--- js/maps.js
+++ js/maps.js
@@ -253,8 +253,8 @@ let maps = [{
     maps[currentMap].count += 1;
 
     if (maps[currentMap].count % 300 === 0) {
-      maps[currentMap].fireBalls.push(new FireBall(15.5 * tile1.size, canvas.height - 2 * tile1.size, 0, -8, 25, 0.1));
-      maps[currentMap].fireBalls.push(new FireBall(20.5 * tile1.size, canvas.height - 2 * tile1.size, 0, -8, 25, 0.1));
+      maps[currentMap].fireBalls.push(new FireBall(15.5 * tile1.size, canvas.height - 2 * tile1.size, 0, - tile1.size / 8, tile1.size, 0.1));
+      maps[currentMap].fireBalls.push(new FireBall(20.5 * tile1.size, canvas.height - 2 * tile1.size, 0, - tile1.size / 8, tile1.size, 0.1));
     }
     
     for (let i = 0; i < maps[currentMap].fireBalls.length; i++) {
@@ -326,19 +326,15 @@ let maps = [{
     }
 
     if (maps[2].count % 300 === 0) {
-      maps[2].fireBalls.push(new FireBall(3.5 * tile1.size, canvas.height - 2 * tile1.size, 0, -8, 25, 0.1));
-      maps[2].fireBalls.push(new FireBall(8.5 * tile1.size, canvas.height - 2 * tile1.size, 0, -8, 25, 0.1));
-      maps[2].fireBalls.push(new FireBall(13.5 * tile1.size, canvas.height - 2 * tile1.size, 0, -8, 25, 0.1));
-      maps[2].fireBalls.push(new FireBall(18.5 * tile1.size, canvas.height - 2 * tile1.size, 0, -8, 25, 0.1));
-      maps[2].fireBalls.push(new FireBall(22.5 * tile1.size, canvas.height - 2 * tile1.size, 0, -8, 25, 0.1));
+      maps[2].fireBalls.push(new FireBall(3.5 * tile1.size, canvas.height - 2 * tile1.size, 0, - tile1.size / 8, tile1.size / 2, 0.1));
+      maps[2].fireBalls.push(new FireBall(8.5 * tile1.size, canvas.height - 2 * tile1.size, 0, - tile1.size / 8, tile1.size / 2, 0.1));
+      maps[2].fireBalls.push(new FireBall(13.5 * tile1.size, canvas.height - 2 * tile1.size, 0, - tile1.size / 8, tile1.size / 2, 0.1));
+      maps[2].fireBalls.push(new FireBall(18.5 * tile1.size, canvas.height - 2 * tile1.size, 0, - tile1.size / 8, tile1.size / 2, 0.1));
+      maps[2].fireBalls.push(new FireBall(22.5 * tile1.size, canvas.height - 2 * tile1.size, 0, - tile1.size / 8, tile1.size / 2, 0.1));
     }
 
     if (maps[2].count % 900 === 0) {
-      maps[2].fireBalls.push(new FireBall(canvas.width, canvas.height - 6 * tile1.size, -6, -0, 25));
-      maps[2].fireBalls.push(new FireBall(canvas.width, canvas.height - 6 * tile1.size, -6, -0, 25));
-      maps[2].fireBalls.push(new FireBall(canvas.width, canvas.height - 6 * tile1.size, -6, -0, 25));
-      maps[2].fireBalls.push(new FireBall(canvas.width, canvas.height - 6 * tile1.size, -6, -0, 25));
-      maps[2].fireBalls.push(new FireBall(canvas.width, canvas.height - 6 * tile1.size, -6, -0, 25));
+      maps[2].fireBalls.push(new FireBall(canvas.width, canvas.height - 6 * tile1.size, -6, -0, tile1.size / 2));
     }
 
     maps[2].count += 1;
